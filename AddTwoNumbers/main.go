630e0bdae126dcb8d6e80444e431833155f07a34
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2024-09-13 00:55:25 +0900

    fix: 🐛 Add Two Numbers

---
+++ AddTwoNumbers/main.go
@@ -0,0 +1,70 @@
+package main
+
+func main() {}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var currentL1, currentL2, firstNode, prevNode *ListNode
+	currentL1 = l1
+	currentL2 = l2
+	var carry, i int
+
+	for {
+		if shouldFinish(currentL1, currentL2, carry) {
+			break
+		}
+
+		var l1Val, l2Val int
+		if currentL1 != nil {
+			l1Val = currentL1.Val
+		}
+		if currentL2 != nil {
+			l2Val = currentL2.Val
+		}
+
+		sum := l1Val + l2Val + carry
+		carry = 0
+		var newVal int
+		// 桁が変わるならキャリーと値を分割
+		if sum > 9 {
+			newVal = sum % 10
+			carry = (sum - newVal) / 10
+		} else {
+			newVal = sum
+		}
+
+		newNode := createNode(newVal, nil)
+
+		if i == 0 {
+			firstNode = newNode
+		} else {
+			prevNode.Next = newNode
+		}
+
+		if currentL1 != nil {
+			currentL1 = currentL1.Next
+		}
+		if currentL2 != nil {
+			currentL2 = currentL2.Next
+		}
+		prevNode = newNode
+		i++
+	}
+
+	return firstNode
+}
+
+func shouldFinish(l1, l2 *ListNode, carry int) bool {
+	return l1 == nil && l2 == nil && carry == 0
+}
+
+func createNode(val int, nextNode *ListNode) *ListNode {
+	return &ListNode{
+		Val:  val,
+		Next: nextNode,
+	}
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 ## 1. Two Sum
 
+## 2. Add Two Numbers
